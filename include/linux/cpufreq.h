83c5a4fb8232b923958294c55d211b910d8b0f5b
Shankar <mathkid95@gmail.com>
2012-01-04 19:20:02 -0600

    Add Lulzactive governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -370,6 +370,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_MINMAX)
 extern struct cpufreq_governor cpufreq_gov_minmax;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_minmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
+extern struct cpufreq_governor cpufreq_gov_lulzactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lulzactive)
 #endif
 
 
