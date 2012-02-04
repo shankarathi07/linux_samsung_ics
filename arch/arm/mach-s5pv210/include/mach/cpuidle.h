6cd25bcf9505903a4027c076d226e3350f5117d7
Shankar <mathkid95@gmail.com>
2012-02-03 23:58:06 -0600

    Revert "Fix the deep idle v2"
    This reverts commit f72627782972399be0fda70da75b47dc1b639a0b.

--- arch/arm/mach-s5pv210/include/mach/cpuidle.h
+++ arch/arm/mach-s5pv210/include/mach/cpuidle.h
@@ -8,7 +8,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
- */
+*/
 
 extern int  s5pv210_didle_save(unsigned long *saveblk);
 extern void s5pv210_didle_resume(void);
