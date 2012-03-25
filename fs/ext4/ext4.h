6c2113ed1ed944a3581029b110b40c1d6d84cc71
Shankar <mathkid95@gmail.com>
2012-03-24 21:37:50 -0500

    Fix compile error

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -1122,6 +1122,7 @@ struct ext4_sb_info {
 	int s_def_hash_version;
 	int s_hash_unsigned;	/* 3 if hash should be signed, 0 if not */
     int s_dirty;
+    struct percpu_counter s_freeclusters_counter;
 	struct percpu_counter s_freeblocks_counter;
 	struct percpu_counter s_freeinodes_counter;
 	struct percpu_counter s_dirs_counter;
